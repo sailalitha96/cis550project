f42c5ddc730a93ce77a51dca9936fe78446d6fac
Rutuja Moharil <rutuja1595@gmail.com>
2020-04-13 14:50:02 -0400

    Some changes

--- predict_ext.py
+++ predict_ext.py
@@ -4,7 +4,7 @@
 Preprocessing :
 '''
 import csv
-import  pandas as pd
+import pandas as pd
 import numpy as np
 
 
@@ -16,16 +16,18 @@ def remove_null_dental_col(filename):
     Columns with Null entries for DentalOnlyPlan Header are populated with "Yes" or "No"
     You may add or remove arguments as necessary; include a description in the README.
     '''
-    network_df = pd.read_csv(filename,keep_default_na = False)
+    network_df = pd.read_csv(filename, keep_default_na=False)
     print("Reading File")
     dental_df = network_df["DentalOnlyPlan"]
-    for i in range(len(dental_df)) :
+    for i in range(len(dental_df)):
         if not len(dental_df[i]):
-            if "Dental" in network_df["NetworkName"][i] :
+            if "Dental" in network_df["NetworkName"][i]:
                 dental_df[i] = "Yes"
             else:
                 dental_df[i] = "No"
     network_df.to_csv(filename)
     print("Done")
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     remove_null_dental_col('Network_sample.csv')
