59e32f5492a486a7ae935b0d6e2b0a8d8e366311
sailalitha96 <gsailalithaa@gmail.com>
2020-05-04 20:06:21 -0400

    change in query

--- backend-controllers/benefit.js
+++ backend-controllers/benefit.js
@@ -17,12 +17,18 @@ exports.getbenefitname = function(req, res) {
     var age= req.params.age; 
     age = JSON.stringify(age);
     // console.log("getagedistinct");
-    var query =`
-    WITH R1 AS  (SELECT DISTINCT(planid) AS plans FROM rate  WHERE issuerid = ${issuerid}  AND Age LIKE ${age}  GROUP BY PlanId), 
-R2 AS  (SELECT DISTINCT(R1.plans) as plans,(bcs.BenefitName) AS Benefits FROM bcs  JOIN R1 ON R1.plans = bcs.StandardComponentId) ,
-R3 AS (SELECT DISTINCT(R1.plans) AS plans, pcw.MetalLevel_2018 AS MetalLevel FROM planidcrosswalk pcw JOIN R1 ON pcw.PlanID_2018 = R1.plans)
+//     var query =`
+//     WITH R1 AS  (SELECT DISTINCT(planid) AS plans FROM rate  WHERE issuerid = ${issuerid}  AND Age LIKE ${age}  GROUP BY PlanId), 
+// R2 AS  (SELECT DISTINCT(R1.plans) as plans,(bcs.BenefitName) AS Benefits FROM bcs  JOIN R1 ON R1.plans = bcs.StandardComponentId) ,
+// R3 AS (SELECT DISTINCT(R1.plans) AS plans, pcw.MetalLevel_2018 AS MetalLevel FROM planidcrosswalk pcw JOIN R1 ON pcw.PlanID_2018 = R1.plans)
 
-SELECT R2.Benefits FROM R2 JOIN R3 ON R2.Plans = R3.Plans GROUP BY R2.Benefits ORDER BY COUNT(DISTINCT(R3.MetalLevel)) DESC LIMIT 10;
-    `;  
+// SELECT R2.Benefits FROM R2 JOIN R3 ON R2.Plans = R3.Plans GROUP BY R2.Benefits ORDER BY COUNT(DISTINCT(R3.MetalLevel)) DESC LIMIT 10;
+//     `;  
+
+    var query = `
+    WITH R2 AS  (SELECT DISTINCT(StandardComponentId) as plans,(bcs.BenefitName) AS Benefits FROM bcs WHERE issuerid = ${issuerid} ) ,
+    R3 AS (SELECT DISTINCT(pcw.PlanId_2018) AS plans, pcw.MetalLevel_2018 AS MetalLevel FROM planidcrosswalk pcw WHERE IssuerId_2018 = ${issuerid} )
+    SELECT R2.Benefits AS Benefits FROM R2 JOIN R3 ON R2.plans = R3.plans GROUP BY Benefits ORDER BY COUNT(DISTINCT(R3.MetalLevel)) DESC LIMIT 10;
+    `
     sql.execute(query, res);
     };
